671144e1a05938fbfa443840fe36efda767e322f
wyhong3103 <89289858+wyhong3103@users.noreply.github.com>
2022-01-11 21:42:26 +0800

    Update selenium_header.py

--- selenium_header.py
+++ selenium_header.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument('headless')
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
 chrome = webdriver.Chrome(executable_path= "your path", options = options)
