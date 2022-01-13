9e7a3b8b746ec928b0fe47f8f0fd7b08a5c2ff35
wyhong3103 <89289858+wyhong3103@users.noreply.github.com>
2022-01-13 16:18:56 +0800

    Update selenium_header.py

--- selenium_header.py
+++ selenium_header.py
@@ -10,4 +10,6 @@ options.add_argument('--ignore-ssl-errors')
 options.add_argument('--headless')
 options.add_argument("--start-maximized")
 options.add_argument("--window-size=1920,1080")
+#binary_location helps to fix some of the bugs, so I decided to add it here for safety reasons
+options.binary_location = "YOUR PATH TO YOUR CHROME BROWSER"
 chrome = webdriver.Chrome(executable_path= "YOUR PATH TO CHROMEDRIVER.EXE", options = options)
