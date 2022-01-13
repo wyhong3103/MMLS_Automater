5e724023d181c73a8da82af7f54455d03f12bad5
wyhong3103 <89289858+wyhong3103@users.noreply.github.com>
2022-01-13 16:20:02 +0800

    Update page.py

--- Pages/page.py
+++ Pages/page.py
@@ -2,12 +2,11 @@ import json
 from Locators.general_locators import GeneralLocators
 from Locators.page_locators import PageLocators
 from Parsers.parser import Parser
-from typing import Generator, List
+from typing import Dict, Generator, List
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 import logging
 
 class Page:
@@ -15,7 +14,7 @@ class Page:
         self.page = page
         self.page.get("https://mmls2.mmu.edu.my/")
 
-    def _getIdPw(self) -> List:
+    def getIdPw(self) -> List:
         """
         This function essentially asks for username and password, if it isn't exist in the json file.
         """
@@ -25,7 +24,6 @@ class Page:
             logging.info("Retrieved user information from JSON and checking if it's empty.")
             if userinfo["username"] and userinfo["password"]:
                 has_info = True
-            
             
         if not has_info:
             userinfo ={ "username" : input("Please enter your student ID: "),
@@ -37,6 +35,9 @@ class Page:
         return [userinfo["username"],userinfo["password"]]
 
     def isElementPresent(self,locator):
+        """
+        This function check if a certain element exists.
+        """
         try:
             logging.info(f"Checking if \"{locator}\" present.")
             self.page.find_element_by_css_selector(locator)
@@ -44,10 +45,13 @@ class Page:
         except:
             return False
 
-    def _login(self):
+    def login(self):
+        """
+        This function helps to login to the MMLS site.
+        """
         loginSuccessful = False
         while not loginSuccessful:
-            username,password = self._getIdPw()
+            username,password = self.getIdPw()
             username_login = self.page.find_element(By.CSS_SELECTOR,GeneralLocators.LOGIN_ID)
             userpw_login = self.page.find_element(By.CSS_SELECTOR,GeneralLocators.LOGIN_PASSWORD)
         
@@ -72,32 +76,44 @@ class Page:
                         userinfo = {"username":"","password":""}
                         json.dump(userinfo,json_file)
 
-
-
-            
-
-
-
     @property
-    def _subjectGetter(self) -> Generator:
+    def subjectGetter(self) -> Generator:
+        """
+        This function grabs all the html tags of the subject cards.
+        """
         logging.info("Getting all the subject cards as an element tag.")
         subject_cards = GeneralLocators.SUBJECT_CARD
         card_list = self.page.find_elements(By.CSS_SELECTOR,subject_cards)
         return card_list
 
-    def view_all_new(self,announcements,subjects):
+    def view_all_new(self,announcements : Dict,subjects : List):
+        """
+        This function is called when you are trying to view all new announcements.
+        """
         logging.info("Viewing all new announcements from all subjects.")
+
+        no_new_announcments_counter = 0
+        #To track how many subject there are, that have no new announcements
+
         for i in subjects:
-            print(i)
-            print("----------------------")
             numberOfNew = announcements[i]["numberOfNew"]
-            print(f"New Announcements: {numberOfNew}\n")
+            if numberOfNew:
+                #Only prints new announcements of subject that has it
+                print(f"\n{i}")
+                print("----------------------")
+                print(f"New Announcements: {numberOfNew}")
 
-            announcements_list = announcements[i]["announcements"]
-            print("\n-----------\n"+"\n-----------\n".join([announcements_list[x] for x in range(len(announcements_list)-1,(len(announcements_list)-numberOfNew)-1,-1)])+"\n-----------\n")
+                announcements_list = announcements[i]["announcements"]
+                print("\n-----------\n"+"\n-----------\n".join([announcements_list[x] for x in range(len(announcements_list)-1,(len(announcements_list)-numberOfNew)-1,-1)])+"\n-----------\n")
+            else:
+                no_new_announcments_counter += 1
+        if no_new_announcments_counter == len(subjects):
+            print("\nThere are no new announcements!\n")
 
-
-    def view_new_announcements(self,announcements,subjectName,all_or_new):
+    def view_new_announcements(self,announcements : Dict,subjectName : str,all_or_new : str):
+        """
+        This function is called when you trying to view a subject's announcements.
+        """
         logging.info(f"Viewing announcements from {subjectName}.")
         print(subjectName)
         if all_or_new == "a":
@@ -111,7 +127,10 @@ class Page:
             print("\n-----------\n"+"\n-----------\n".join([announcements_list[x] for x in range(len(announcements_list)-1,(len(announcements_list)-numberOfNew)-1,-1)])+"\n-----------\n")
             
 
-    def select_subject(self,announcements,subjects):
+    def select_subject(self,announcements : Dict ,subjects : List):
+        """
+        This function is called when you want to view announcements of a specific subject, and it let you select one of them.
+        """
         logging.info("Selecting subject to be view.")
         for i,j in enumerate(subjects):
             print(f"{i+1} : {j}")
@@ -129,18 +148,21 @@ class Page:
                 print("\n")
             else:
                 print("Invalid input")
-
         
+
         #-1 in the index of subejcts because we have added 1 to each of the index, in the last operation
         self.view_new_announcements(announcements,subjects[subjectID-1],all_or_new)
 
 
 
     def subjects_checker(self):
+        """
+        This function checks for new announcements, and provide the menu for navigation.
+        """
         subjects = []
-        for i in range(len(self._subjectGetter)):
+        for i in range(len(self.subjectGetter)):
             #rerendering 
-            element = self._subjectGetter[i]
+            element = self.subjectGetter[i]
             #i.text somehow return the name of the subject without having you find it again with css selector
             subjectName = element.text
             logging.info(f"{subjectName} is being checked for new announcements.")
