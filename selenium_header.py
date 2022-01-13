eec01a1507eb2f9caf8f86a4f6d6a0ff94e0cd00
wyhong3103 <89289858+wyhong3103@users.noreply.github.com>
2022-01-13 16:17:41 +0800

    Update selenium_header.py

--- selenium_header.py
+++ selenium_header.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
-chrome = webdriver.Chrome(executable_path= "your path", options = options)
-
-
+#headless, start-maximzed, window-size somehow fixed element not interable problem!
+options.add_argument('--headless')
+options.add_argument("--start-maximized")
+options.add_argument("--window-size=1920,1080")
+chrome = webdriver.Chrome(executable_path= "YOUR PATH TO CHROMEDRIVER.EXE", options = options)
