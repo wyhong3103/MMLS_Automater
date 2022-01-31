4cf4c4e50dff13b0357c687075b1a5f5ae279075
wyhong3103 <89289858+wyhong3103@users.noreply.github.com>
2022-01-31 18:56:33 +0800

    Update selenium_header.py

--- selenium_header.py
+++ selenium_header.py
@@ -6,8 +6,10 @@ options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
-#headless, start-maximzed, window-size somehow fixed element not interable problem!
+#headless, start-maximzed, window-size somehow fixed element not interactable problem!
 options.add_argument('--headless')
 options.add_argument("--start-maximized")
 options.add_argument("--window-size=1920,1080")
-chrome = webdriver.Chrome(executable_path= "C:\\Users\\ACER\\Desktop\\c0ding\\python\\webscraping\\chromedriver.exe", options = options)
+#binary_location helps to fix some of the bugs, so I decided to add it here for safety reasons
+options.binary_location = "YOUR PATH TO YOUR CHROME BROWSER"
+chrome = webdriver.Chrome(executable_path= "YOUR PATH TO CHROMEDRIVER.EXE", options = options)
