e2676844d08d1f2c7f065f12e9d829a1e26e899b
wyhong3103 <89289858+wyhong3103@users.noreply.github.com>
2022-01-31 19:10:25 +0800

    Update app.py
    Added functionality that create "logs" directory, if it didn't exist

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from menu import menu
 from datetime import datetime
+import os
 import logging
 
+directory_list = os.listdir()
+if "logs" not in directory_list:
+    logging.info("Creating logs directory")
+    os.mkdir("logs")
 date = datetime.today().strftime("%d-%m-%y--%I-%M")
 logging.basicConfig(format = "%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",level = logging.INFO, filename = f"logs\\logs-{date}.txt")
 
